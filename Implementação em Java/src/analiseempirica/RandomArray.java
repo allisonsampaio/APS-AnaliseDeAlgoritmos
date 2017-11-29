7f2cdb17fec0391079d8b83f433c8437304ec1dc
rodps <rodrigopds73@gmail.com>
2017-11-29 10:23:09 -0200

    revertido random array para sem % de negativos

--- Implementação em Java/src/analiseempirica/RandomArray.java
+++ Implementação em Java/src/analiseempirica/RandomArray.java
@@ -14,19 +14,10 @@ public class RandomArray {
         r.setSeed(seed);
     }
     
-    public int[] generate(int length, int bound, double percent){
+    public int[] generate(int length, int bound){
         int[] array = new int[length];
-        int cont = 0;
-        int i;
-        for (i = 0; i < length; i++){
-            array[i] = r.nextInt(bound)+1;
-        }
-        while(cont < (int)(percent*length)){
-            i = r.nextInt(length);
-            if(array[i] > 0){
-                array[i] *= -1;
-                cont++;
-            }
+        for (int i = 0; i < length; i++){
+            array[i] = r.nextInt(bound*2)-bound;
         }
         return array;
     }
