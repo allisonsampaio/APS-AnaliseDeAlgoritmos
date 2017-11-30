45de7113768b85f56c693d6d89cb0c35186d37c8
allisonsampaio <allisonsampaiox@gmail.com>
2017-11-30 16:48:26 -0200

    implementação em c

--- Implementação em C/enumeration.c
+++ Implementação em C/enumeration.c
@@ -1,4 +1,4 @@
-// Subarray Máximo - Linear
+// Subarray Máximo - Enumeration
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -18,33 +18,21 @@ int max(int a, int b)
 
 int max_subarray(int array[], int n)
 {
-	int i;
-	int aux = array[0];
-	int sum = 0;
-
-	for(i = 1; i<n; i++) //Verifique se todos são negativos
+	int ans = INT_MIN;
+	int inicio;
+	for(inicio = 0; inicio < n; inicio++)	
 	{
-		aux = max(aux, array[i]);
+		int sum = 0;
+		int tam;
+		for(tam = 1; tam <= n; tam++)
+		{
+			if(inicio+tam > n)
+				break;
+			sum = sum + array[inicio + tam-1];
+			ans = max(ans,sum);
+		}
 	}
-
-	if(aux<0){ //se max < 0 retorna max
-		return aux;
-	}
-
-	aux = 0;
-
-	for(i = 0 ; i<n; i++)
-	{
-		if(sum + array[i] > 0)
-			sum= sum + array[i];
-
-		else
-			sum  = 0;
-
-		aux = max(aux,sum);
-	}
-
-	return aux;
+	return ans;
 }
 
 int arquivo1(int tam){
@@ -135,13 +123,27 @@ int arquivo5(int tam){
 int main(int argc, char const *argv[])
 {
 
+	struct timeval time1, time2;
+    double elapsedTime; // the amount of time that has passed since a particular process started
+
+	clock_t t1=clock();
+	gettimeofday(&time1, NULL);
+
 	int tam = 10000; //Tamanho máximo de elementos em cada entrada: 1048576
 
 	printf("%d\n",arquivo1(tam));
-	printf("%d\n",arquivo2(tam));
-	printf("%d\n",arquivo3(tam));
-	printf("%d\n",arquivo4(tam));
-	printf("%d\n",arquivo5(tam));
+	//printf("%d\n",arquivo2(tam));
+	//printf("%d\n",arquivo3(tam));
+	//printf("%d\n",arquivo4(tam));
+	//printf("%d\n",arquivo5(tam));
+
+	clock_t t2=clock();
+	gettimeofday(&time2, NULL);
+	elapsedTime = (time2.tv_sec - time1.tv_sec) * 1000.0;      // // converte segundos to ms
+    elapsedTime += (time2.tv_usec - time1.tv_usec) / 1000.0;   // // converte us to ms
+
+    printf("Tempo de CPU: %lf ms\n", ((double)(1000.0 * (t2-t1) / CLOCKS_PER_SEC)));
+	printf("Tempo real: %lf ms\n", elapsedTime);
 
 	return 0;
 }
